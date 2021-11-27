9ed7103c3478bb3027d879487e50f356ae2596fc
qwarl <quannngoc98@gmail.com>
2021-11-27 19:49:43 +0700

    navigation bug

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import Navigation from './navigation/Navigation';
-
+import Login from './screens/Login';
 export default function App() {
   return (
     <View style={styles.container}>
-     
       <NavigationContainer>
         <Navigation />
       </NavigationContainer>
+      {/* <Login /> */}
+      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from '../screens/Home';
 import Login from '../screens/Login';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
@@ -15,14 +14,14 @@ const Navigation = () => {
         <Stack.Navigator initialRouteName='Login' screenOptions={screenOptions}>
             <Stack.Screen name='Login' component={Login} screenOptions={screenOptions} />
             <Stack.Screen name='WelcomeScreen' component={WelcomeScreen} screenOptions={screenOptions} />
-            {/* <Stack.Screen name='BrandsContainer' component={BrandsContainer} screenOptions={screenOptions} />
-            <Stack.Screen name='ProductDetail' component={ProductDetail} screenOptions={screenOptions} />
+            {/* <Stack.Screen name='BrandsContainer' component={BrandsContainer} screenOptions={screenOptions} /> */}
+            {/* <Stack.Screen name='ProductDetail' component={ProductDetail} screenOptions={screenOptions} />
             <Stack.Screen name='Search' component={Search} screenOptions={screenOptions} /> */}
         </Stack.Navigator>
 
     )
 }
 
-export default { Navigation }
+export default Navigation;
 
 const styles = StyleSheet.create({})
